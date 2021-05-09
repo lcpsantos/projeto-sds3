6e3cf11702698012aa39dc9bcc2511875efbfc52
Luiz César Pacheco dos Santos <lcpsantos@gmail.com>
2021-05-09 14:39:12 -0300

    First request

--- frontend/.history/src/components/DonutChart/index_20210509142322.tsx
+++ frontend/.history/src/components/DonutChart/index_20210509142322.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import Chart from "react-apexcharts";
-import { SaleSum } from 'types/sale';
 import { BASE_URL } from "utils/requests";
 
 type ChartData = {
@@ -9,19 +8,12 @@ type ChartData = {
 };
 
 const DonutChart = () => {
-
   //FORMA ERRADA
   let chartData: ChartData = { labels: [], series: [] };
 
-  //FORMA ERRADA
   axios.get(`${BASE_URL}/sales/amount-by-seller`)
       .then((response) => {
-          const data = response.data as SaleSum[];
-          const myLabels = data.map(x => x.sellerName);
-          const mySeries = data.map(x => x.sum);
-
-          chartData = { labels: myLabels, series: mySeries};
-          console.log(chartData);
+          console.log(response.data);
   });
 
   //const mockData = {
