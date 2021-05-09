7d13054c058d71fade5b17648e1ace42c1383ecb
Luiz César Pacheco dos Santos <lcpsantos@gmail.com>
2021-05-09 00:21:44 -0300

    Routes

---
+++ frontend/.history/src/App_20210509001426.tsx
@@ -0,0 +1,13 @@
+import Navbar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
